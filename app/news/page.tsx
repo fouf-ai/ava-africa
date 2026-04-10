38a885aac7ad29e30342e9f91ad05329efcfcbe3
itsladykerrtoo <foufkagna@bsu.edu.az>
2026-04-11 01:25:18 +0400

    fix use client order news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
 "use client";
+
+import { useState } from "react";
+import Link from "next/link";
 
 import Link from "next/link";
 import Header from "../components/Header";
